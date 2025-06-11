78b60841fb59501c4c87e005117da316769cdaf5
davut <davuthankurt@gmail.com>
2025-06-11 14:00:42 +0300

    extended filters and created filter arrays

--- ToonVerse/AI/AIFilter.swift
+++ ToonVerse/AI/AIFilter.swift
@@ -20,7 +20,17 @@ enum AIFilter: CaseIterable, Identifiable {
     case cyberpunk
     case gta
     case pixels
-
+    case filmPoster
+    case urbanFashion
+    case boxer
+    case gladiator
+    case gothic
+    case superModel
+    case space
+    case romanStatue
+    case tarzan
+    case toy
+    
     var id: UUID {
         UUID()
     }
@@ -49,6 +59,26 @@ enum AIFilter: CaseIterable, Identifiable {
             return "The Game"
         case .pixels:
             return "Pixel-Art"
+        case .filmPoster:
+            return "Film Poster"
+        case .urbanFashion:
+            return "Urban Fashion"
+        case .boxer:
+            return "Boxer"
+        case .gladiator:
+            return "Gladiator"
+        case .gothic:
+            return "Gothic"
+        case .superModel:
+            return "Super Model"
+        case .space:
+            return "Mars"
+        case .romanStatue:
+            return "Roman Statue"
+        case .tarzan:
+            return "Jungle Man"
+        case .toy:
+            return "Become Toy"
         }
     }
 
@@ -76,6 +106,26 @@ enum AIFilter: CaseIterable, Identifiable {
             return .gta
         case .pixels:
             return .pixelArt
+        case .filmPoster:
+            return .filmPoster
+        case .urbanFashion:
+            return .urbanFashion
+        case .boxer:
+            return .boxer
+        case .gladiator:
+            return .gladiator
+        case .gothic:
+            return .gothic
+        case .superModel:
+            return .superModel
+        case .space:
+            return .space
+        case .romanStatue:
+            return .romanStatue
+        case .tarzan:
+            return .tarzan
+        case .toy:
+            return .toy
         }
     }
 
@@ -92,9 +142,9 @@ enum AIFilter: CaseIterable, Identifiable {
         case .popArt:
             return "Make the image pop art style"
         case .portrait:
-            return "Make the image colonial portrait style"
+            return "Make the image colonial portrait style with a frame around."
         case .superhero:
-            return "Make the image comics superhero style"
+            return "Make the image superhero style"
         case .wildWest:
             return "Make the image wild west style"
         case .cyberpunk:
@@ -103,8 +153,44 @@ enum AIFilter: CaseIterable, Identifiable {
             return "Make the image gta style"
         case .pixels:
             return "Make the image 2d pixel-art game style"
+        case .filmPoster:
+            return "Make the image film poster style"
+        case .urbanFashion:
+            return "Make the image gta style"
+        case .boxer:
+            return "Make the image a boxing scene style"
+        case .gladiator:
+            return "Make the image gladiator style"
+        case .gothic:
+            return "Make the image gothic style"
+        case .superModel:
+            return "Make the image super model style"
+        case .space:
+            return "Make the photo look like it was taken on Mars"
+        case .romanStatue:
+            return "Make the image ancient Roman statue style"
+        case .tarzan:
+            return "Make the image Tarzan jungle style"
+        case .toy:
+            return "Make the image lego style"
         }
     }
-    
-    
 }
+
+extension AIFilter {
+    static var defaultFilters: [AIFilter] {
+        return [.middleEarth, .popArt, .manga, .portrait, .superhero, .urbanFashion]
+    }
+    
+    static var mustTry: [AIFilter] {
+        return [.cartoon, .toy, .gothic, .bubbleHead, .space, .wildWest, .filmPoster]
+    }
+
+    static var landscapeFilters: [AIFilter] {
+        return [.pixels, .superModel, .tarzan]
+    }
+    
+    static var portraitFilters: [AIFilter] {
+        return [.boxer, .cyberpunk, .gta, .gladiator, .romanStatue]
+    }
+} 
