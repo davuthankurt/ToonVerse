481034eef83bf449d45c2035acbfa4e78f069646
davut <davuthankurt@gmail.com>
2025-07-10 00:04:22 +0300

    usermodel update

--- ToonVerse/Models/UserModel.swift
+++ ToonVerse/Models/UserModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import RevenueCat
+
+class UserModel: ObservableObject {
+    @Published var isSubscriptionActive = false
+    
+    init() {
+        Purchases.shared.getCustomerInfo { (customerInfo, error) in
+            if customerInfo?.entitlements.all["Pro"]?.isActive == true {
+                // User is "premium"
+                self.isSubscriptionActive = true
+            }
+        }
+    }
+}
