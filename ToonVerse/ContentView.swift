e06571396ed0a709a86f6d0627914288c283f135
davut <davuthankurt@gmail.com>
2025-07-10 00:17:55 +0300

    splash screen and navbar appearance configurations

--- ToonVerse/ContentView.swift
+++ ToonVerse/ContentView.swift
@@ -10,20 +10,78 @@ import PhotosUI
 import NavigatorUI
 
 struct ContentView: View {
+    @State private var isActive = false
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(Color.myBackground)
+        appearance.shadowColor = .clear
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+        ]
+        appearance.largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().tintColor = .white
+    }
+}
+
+extension ContentView {
     var body: some View {
-        ManagedNavigationStack {
-            HomeScreen()
-                .navigationDestination(HomeDestinations.self)
-                .navigationTitle("ToonVerse")
-                .toolbarBackground(Color.myBackground, for: .navigationBar)
-                .toolbarBackground(.visible, for: .navigationBar)
-                .toolbarColorScheme(.dark, for: .navigationBar)
-                .preferredColorScheme(.dark)
+        Group {
+            if isActive {
+                ManagedNavigationStack {
+                    HomeScreen()
+                        .navigationDestination(HomeDestinations.self)
+                        .navigationDestination(GenerateDestination.self)
+                        .navigationTitle("ToonStellar")
+                        .preferredColorScheme(.dark)
+                }
+                .tint(.white)
+                
+            } else {
+                ZStack {
+                    VStack {
+                        Spacer()
+                        Image(.toonverseLogo)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200)
+                            .clipShape(.rect(cornerRadius: 24))
+                            
+                            .padding()
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .background(
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                        Color(red: 171/255, green: 167/255, blue: 243/255),
+                        .myBlue
+                    ]),
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            isActive = true
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
 #Preview {
+    var userModel = UserModel()
     ContentView()
+        .environmentObject(userModel)
 }
 
