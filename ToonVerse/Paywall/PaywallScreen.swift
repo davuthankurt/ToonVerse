c9d00c6a352c5c6d7cc80d5bd8b15a1e2ecdcdf0
davut <davuthankurt@gmail.com>
2025-07-10 00:08:31 +0300

    paywall view and subscriptions

--- ToonVerse/Paywall/PaywallScreen.swift
+++ ToonVerse/Paywall/PaywallScreen.swift
@@ -6,24 +6,54 @@
 //
 
 import SwiftUI
+import RevenueCat
 
 struct PaywallScreen: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.openURL) var openURL
+    @EnvironmentObject var userModel: UserModel
     @State private var colorChange = false
-//    Temporary paywall screen
+    @State private var currentOffering: Offering?
+    @State private var selectedPackage: Package?
+
     var body: some View {
         GeometryReader { proxy in
-            VStack {
+            VStack(spacing: 10) {
                 alignmentStacks(proxy)
-                subscriptionSelection("$7.99/weak", "Try all features with full flexibility.\nCancel anytime.")
-                subscriptionSelection("$35.99/year", "Save over 50%.\nOne-time payment for a full year of access.")
-                
-                subscriptionButton
                 Spacer()
-                policyButton
+                Text("Renews automatically. Cancel anytime.")
+                    .foregroundStyle(.gray)
+                    .frame(maxWidth: .infinity)
+                    .font(.headline)
+                if currentOffering != nil {
+                    ForEach(currentOffering!.availablePackages) { pkg in
+                        subscriptionSelection(pkg)
+                            .padding(.horizontal)
+                    }
+                }
+                subscriptionButton
+                    .disabled(selectedPackage == nil)
                 
+
+                HStack(alignment: .center) {
+                    Spacer()
+                    termsButton
+                    Spacer()
+                    policyButton
+                    Spacer()
+                    restoreButton
+                    Spacer()
+                }
             }
-            .background(Color.myBackground.ignoresSafeArea())
+            .ignoresSafeArea()
+            .background(.myBackground)
+            .onAppear {
+                Purchases.shared.getOfferings { offerings, error in
+                    if let offer = offerings?.current, error == nil {
+                        currentOffering = offer
+                    }
+                }
+            }
         }
     }
     
@@ -36,21 +66,24 @@ struct PaywallScreen: View {
                     Spacer()
                     realImage
                 }
+                .padding(.vertical)
             }
             headerText
         }
+        .padding(.vertical)
     }
     
     private func backgroundImage(_ proxy: GeometryProxy) -> some View {
-        Image(.image7)
+        let heightRatio = proxy.size.height < 700 ? 0.5 : 0.6
+        return Image(.image7)
             .resizable()
             .scaledToFill()
-            .frame(width: proxy.size.width, height: proxy.size.height / 2)
+            .frame(width: proxy.size.width, height: proxy.size.height * heightRatio)
             .clipped()
             .overlay {
                 LinearGradient(
                     stops: [
-                        .init(color: .clear, location: 0.7),
+                        .init(color: .clear, location: 0.8),
                         .init(color: .myBackground, location: 1.0)
                     ],
                     startPoint: .top,
@@ -59,7 +92,7 @@ struct PaywallScreen: View {
             }
             .ignoresSafeArea()
     }
-
+    
     private var realImage: some View {
         Image(.landingOriginal)
             .resizable()
@@ -87,7 +120,7 @@ struct PaywallScreen: View {
 
     private var headerText: some View {
         VStack {
-            Text("ToonVerse Pro")
+            Text("ToonStellar Pro")
                 .font(.title.bold())
                 .foregroundStyle(.white)
             
@@ -98,19 +131,35 @@ struct PaywallScreen: View {
         .padding(.vertical, -10)
     }
     
-    private func subscriptionSelection(_ title: String, _ subText: String) -> some View {
-        Button {
-            
+    private func subscriptionSelection(_ pkg: Package) -> some View {
+        let isSelected = selectedPackage?.identifier == pkg.identifier
+        
+        return Button {
+            selectedPackage = pkg
         } label: {
-            VStack(alignment: .leading) {
-                Text(title)
+            HStack {
+                Circle()
+                    .stroke(isSelected ? .myBlue : .white.opacity(0.2), lineWidth: 2)
+                    .frame(width: 24, height: 24)
+                    .overlay {
+                        if isSelected {
+                            Image(systemName: "checkmark.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.myBlue)
+                        }
+                    }
+                    
+                Text(pkg.storeProduct.subscriptionPeriod!.durationTitle)
                     .font(.title3.bold())
                     .foregroundStyle(.white)
                 
-                Text(subText)
-                    .font(.subheadline)
-                    .foregroundStyle(.myBlue)
-                    .multilineTextAlignment(.leading)
+                Spacer()
+                
+                Text(pkg.storeProduct.localizedPriceString)
+                    .font(.title3.bold())
+                    .foregroundStyle(.white)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding()
@@ -120,35 +169,62 @@ struct PaywallScreen: View {
         .clipShape(.rect(cornerRadius: 10))
         .overlay {
             RoundedRectangle(cornerRadius: 10)
-                .stroke(.myBlue.opacity(0.5), lineWidth: 2)
+                .stroke(isSelected ? .myBlue : .white.opacity(0.2), lineWidth: 2)
         }
-        
-        .padding()
+        .shadow(color: .myBlue,
+                radius: isSelected ? 5 : 0)
     }
     
     private var subscriptionButton: some View {
         Button {
-            
-        } label: {
-            HStack {
-                Text("Hello World")
-                    .foregroundStyle(.white)
+            if let pkg = selectedPackage {
+                Purchases.shared.purchase(package: pkg) { transaction, customerInfo, error, userCancelled in
+                    if customerInfo?.entitlements.all["Pro"]?.isActive == true {
+                        userModel.isSubscriptionActive = true
+                        dismiss()
+                    }
+                }
             }
-            .frame(maxWidth: .infinity, maxHeight: 50)
-            .background(Color.myBackground)
-            .clipShape(Capsule())
-            .fontWeight(.bold)
-            .padding()
-            .shadow(
-                color: .myBlue,
-                radius: 5
-            )
+        } label: {
+            Text("Continue")
+                .font(.title2.bold())
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
         }
+        .background(
+            Capsule()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color(red: 171/255, green: 167/255, blue: 243/255),
+                            .myBlue
+                        ]),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                )
+            )
+        )
+        .padding([.horizontal, .top])
     }
     
-    private var policyButton: some View{
+    private var termsButton: some View {
         Button {
-            
+            if let url = URL(string: "https://sites.google.com/view/toonstellar-termsconditions/ana-sayfa") {
+                openURL(url)
+            }
+        } label: {
+            Text("Terms")
+                .foregroundStyle(.gray)
+        }
+        .padding()
+    }
+    
+    private var policyButton: some View {
+        Button {
+            if let url = URL(string: "https://sites.google.com/view/toonstellar-privacypolicy/ana-sayfa") {
+                openURL(url)
+            }
         } label: {
             Text("Privacy")
                 .foregroundStyle(.gray)
@@ -156,8 +232,25 @@ struct PaywallScreen: View {
         .padding()
     }
 
+    private var restoreButton: some View {
+        Button {
+            Purchases.shared.restorePurchases { customerInfo, error in
+                // ... check customerInfo to see if entitlement is now active
+                if customerInfo?.entitlements.all["Pro"]?.isActive == true {
+                    userModel.isSubscriptionActive = true
+                    dismiss()
+                }
+            }
+        } label: {
+            Text("Restore")
+                .foregroundStyle(.gray)
+        }
+        .padding()
+    }
 }
 
 #Preview {
+    var usermodel = UserModel()
     PaywallScreen()
+        .environmentObject(usermodel)
 }
