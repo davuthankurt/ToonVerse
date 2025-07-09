1fa71360a6d4fb036322ebba72a66abd3142499a
davut <davuthankurt@gmail.com>
2025-07-10 00:12:33 +0300

    toolbar buttons added

--- ToonVerse/Home/HomeDestinations.swift
+++ ToonVerse/Home/HomeDestinations.swift
@@ -11,15 +11,18 @@ import SwiftUI
 enum HomeDestinations {
     case generatedImageView(UIImage, AIFilter)
     case paywallScreen
+    case settings
 }
 
 extension HomeDestinations: NavigationDestination {
     public var body: some View {
         switch self {
         case .generatedImageView(let image, let filter):
-            GeneratedImageView(image: image, selectedFilter: filter)
+            GeneratedImageView(image: image, selectedFilter: filter, imageToGenerate: image)
         case .paywallScreen:
             PaywallScreen()
+        case .settings:
+            SettingsScreen()
         }
     }
 }
--- ToonVerse/Home/HomeScreen.swift
+++ ToonVerse/Home/HomeScreen.swift
@@ -11,24 +11,72 @@ import NavigatorUI
 
 struct HomeScreen: View {
     @Environment(\.navigator) var navigator
-    @State private var selectionModel = SelectionModel()
+    @EnvironmentObject var userModel: UserModel
+    
+    @StateObject private var selectionModel = SelectionModel()
     @State private var isShowingPicker = false
+    @State private var remainingCredits = KeychainWrapper.standard.integer(forKey: "com.toonverse.remainingCredits") ?? 0
 }
 
 extension HomeScreen {
     var body: some View {
-        ScrollView {
+        ScrollView(showsIndicators: false) {
             LandscapeView(isShowingPicker: $isShowingPicker)
             PortraitView(isShowingPicker: $isShowingPicker)
             SquareView(isShowingPicker: $isShowingPicker)
         }
-        .environment(selectionModel)
+    
+        .environmentObject(selectionModel)
         .photosPicker(isPresented: $isShowingPicker, selection: $selectionModel.selectedItem, matching: .any(of: [.images, .screenshots, .livePhotos]))
-        .onChange(of: selectionModel.selectedItem) { _, newItem in
+        .onChange(of: selectionModel.selectedItem) { newItem in
             selectedItemChanged(newItem)
         }
+        .onFirstAppear {
+            if userModel.isSubscriptionActive == false {
+                navigator.navigate(to: HomeDestinations.paywallScreen, method: .cover)
+            }
+        }
         .onAppear {
-            navigator.navigate(to: HomeDestinations.paywallScreen, method: .cover)
+            remainingCredits = getRemainingCredits()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    navigator.navigate(to: HomeDestinations.settings)
+                } label: {
+                    Image(systemName: "gearshape")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    navigator.navigate(to: HomeDestinations.paywallScreen, method: .cover)
+                } label: {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image(systemName: "bolt")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                        
+                        if !userModel.isSubscriptionActive {
+                            Text("\(remainingCredits)")
+                                .font(.caption2)
+                                .foregroundColor(.white)
+                                .padding(3)
+                                .background(Circle().fill(.myBlue))
+                                .offset(x: 7, y: 3)
+                        } else {
+                            Image(systemName: "infinity.circle.fill")
+                                .symbolRenderingMode(.palette)
+                                .foregroundStyle(.white, .myBlue)
+                                .font(.caption2)
+                                .offset(x: 7, y: 3)
+                        }
+                    }
+                }
+            }
         }
         .background(.myBackground)
     }
@@ -41,13 +89,19 @@ extension HomeScreen {
                let uiImage = UIImage(data: data),
                let prompt = selectionModel.selectedPrompt {
                 navigator.navigate(to: HomeDestinations.generatedImageView(uiImage, prompt))
+                selectionModel.selectedItem = nil
             }
         }
     }
+    func getRemainingCredits() -> Int {
+        return KeychainWrapper.standard.integer(forKey: "com.toonverse.remainingCredits") ?? 0
+    }    
 }
 
 
 
 #Preview {
+    var userModel = UserModel()
     HomeScreen()
+        .environmentObject(userModel)
 }
